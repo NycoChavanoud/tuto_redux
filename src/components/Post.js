1fab4cb06061443c0ab164189191ed99b7b4ea64
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-12-11 17:45:26 +0100

    next steps finish, rest delete and likes

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deletePost, editPost } from "../actions/post.action";
 import Like from "./Like";
+import { isEmpty } from "./Utils";
 import "../styles/post.css";
 
 const Post = ({ post }) => {
+  const user = useSelector((state) => state.userReducer);
+  const [editToggle, setEditToggle] = useState(false);
+  const [editContent, setEditContent] = useState(post.content);
+  const dispatch = useDispatch();
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+
+    const postData = {
+      title: post.title,
+      author: user[0].pseudo,
+      content: editContent,
+      likes: post.likes,
+      id: post.id,
+    };
+    dispatch(editPost(postData));
+    setEditToggle(false);
+  };
+
   return (
     <div className="post">
+      {!isEmpty(user[0]) && user[0].pseudo === post.author && (
+        <div className="edit-delete">
+          <img
+            onClick={() => setEditToggle(!editToggle)}
+            src="./icons/edit.svg"
+            alt="edit"
+          />
+          <img
+            onClick={() => dispatch(deletePost(post.id))}
+            src="./icons/delete.svg"
+            alt="delete"
+          />
+        </div>
+      )}
       <h2>{post.title}</h2>
       <img
         src="https://picsum.photos/1500/400"
@@ -12,8 +48,18 @@ const Post = ({ post }) => {
         alt="img-post"
       />
 
-      <p>{post.content}</p>
-
+      {editToggle ? (
+        <form onSubmit={(e) => handleEdit(e)}>
+          <textarea
+            type="text"
+            defaultValue={post.content}
+            onChange={(e) => setEditContent(e.target.value)}
+          />
+          <input type="submit" value="Valider modification" />
+        </form>
+      ) : (
+        <p>{post.content}</p>
+      )}
       <div className="author">
         <h5>{post.author}</h5>
         <Like post={post} />
@@ -21,5 +67,4 @@ const Post = ({ post }) => {
     </div>
   );
 };
-
 export default Post;
